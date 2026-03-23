89110398395ba865d764b3e37c60ee90c1bf4ccf
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-03-23 12:38:15 +0530

    Variables

--- main.go
+++ main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	var num1 int = 10
+	var num2 int = 20
+
+	var sum int
+
+	sum = num1 + num2
+
+	fmt.Printf("The sum is: %d\n", sum)
 }
