c94709e4a055f064584b15e258f0bb9ffd9a6326
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-03-23 17:40:40 +0530

    Input string

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	power := 1000 // The variable should always be used in the code somewhere
-	value := 100
-	fmt.Printf("The value is: %d\n", value)
-	fmt.Printf("default power is %d\n", power)
-	name, power := "Rishi", 99999
-	fmt.Printf("%s's power is over %d\n", name, power)
+	var name string
+
+	fmt.Print("Enter your name : ")
+	fmt.Scanln(&name)
+
+	fmt.Println("Hello, :", name)
 }
