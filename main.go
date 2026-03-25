f399816576eeb9fdd73c6204c048e6cb92caebca
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-03-25 18:16:45 +0530

    Pointer Example 6 (Method with Pointer Reciever)

--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import "fmt"
 
-func main() {
-	x := 10
-	p := &x
-	pp := &p
+type Saiyan struct {
+	Power int
+}
 
-	fmt.Print(*p)  // returns memory of x
-	fmt.Print(*pp) // returns memory of x
+func (s *Saiyan) PowerUp() {
+	s.Power += 1000
+}
+
+func main() {
+	goku := Saiyan{Power: 9000}
+	goku.PowerUp()
+
+	fmt.Println(goku.Power) // 10000
 }
