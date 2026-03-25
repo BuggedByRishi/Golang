613f32b2a5c6974514c832f940ade211475f7fb1
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-03-25 11:26:52 +0530

    Slicing

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var number [3]int = [3]int{1, 2, 3}
+	nums := []int{1, 2, 3}
 
-	fmt.Print(number)
+	nums = append(nums, 4, 5, 6)
+
+	fmt.Print("\n", nums)    // After appending 4
+	fmt.Print("\n", nums[0]) // Accessing element from array
+	fmt.Print(len(nums))     // Length of array
+	fmt.Print(cap(nums))     // Capacity or size of the array
 }
