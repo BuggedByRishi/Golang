21e6db433ed527ccbb8a8069a89f9174cc9a6ede
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-03-25 12:06:00 +0530

    Loop through Slicing

--- main.go
+++ main.go
@@ -7,8 +7,7 @@ func main() {
 
 	nums = append(nums, 4, 5, 6)
 
-	fmt.Print("\n", nums)    // After appending 4
-	fmt.Print("\n", nums[0]) // Accessing element from array
-	fmt.Print(len(nums))     // Length of array
-	fmt.Print(cap(nums))     // Capacity or size of the array
+	for i, v := range nums { // Loop through Slice
+		fmt.Println(i, v)
+	}
 }
