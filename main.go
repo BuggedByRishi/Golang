20ffe6fa598f90bca0284947781177e82eb8bd77
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-03-25 17:14:58 +0530

    Pointer Example 2

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	x := 10
 	p := &x
 
-	fmt.Print("The value of x is : ", x)          // The actual value
-	fmt.Print("\nThe address of x is : ", p)      // Address of the value
-	fmt.Print("\nValues using pointer is : ", *p) // The value Pointe holds
+	*p = 20 // Modifying value
+
+	fmt.Println(x)
 }
