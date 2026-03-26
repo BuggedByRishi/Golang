c7ed8c106931e3597d8d1fd9cd1558962c2b9c84
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-03-26 16:41:50 +0530

    Structure Example 2

--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import "fmt"
 
+type address struct {
+	city string
+}
+
 type person struct {
-	name string
-	age  int
+	name    string
+	address address
 }
 
 func main() {
-	p := person{name: "Rishi", age: 21}
-	fmt.Println(p.name, p.age)
+	p := person{
+		name: "Rishi",
+		address: address{
+			city: "Pune",
+		},
+	}
+	fmt.Println(p.name, p.address.city)
 }
