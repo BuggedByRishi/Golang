506c87260e849bb80b999de67df45968d5fe0a78
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-03-27 12:58:12 +0530

    Error Handeling Example 1

--- main.go
+++ main.go
@@ -1,31 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-type Payment interface {
-	Pay(amount int)
-}
-
-type upi struct{}
-
-func (u upi) Pay(amount int) {
-	fmt.Println("Paid via UPI: ", amount)
-}
-
-type card struct{}
-
-func (c card) Pay(amount int) {
-	fmt.Println("Paid via Card: ", amount)
-}
-
-func processPayment(p Payment) {
-	p.Pay(10000000000)
+func divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("Cannot divide by zero")
+	}
+	return a / b, nil
 }
 
 func main() {
-	u := upi{} // Object creation			// This is for UPI payment
-	processPayment(u)
+	result, err := divide(10, 0)
 
-	c := card{}       // Object creation
-	processPayment(c) // This if for CARD PAyment
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	fmt.Println("Result: ", result)
 }
