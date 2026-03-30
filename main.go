0591941f140c884b0875a49e99ba7a67b7108c3a
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-03-30 10:33:07 +0530

    Defer: Execution line by line

--- main.go
+++ main.go
@@ -1,33 +1,13 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-)
-
-type User struct {
-	Name string `json:"name"`
-}
-
-func createUser(w http.ResponseWriter, r *http.Request) {
-	var user User
-
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		http.Error(w, "Invalid JSON", http.StatusBadRequest)
-		return
-	}
-
-	json.NewEncoder(w).Encode(user)
-}
+import "fmt"
 
 func main() {
-	http.HandleFunc("/user", createUser)
-
-	fmt.Println("Server running on http://localhost:8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println("Server error:", err)
-	}
+	fmt.Println("Hello")
+	defer fmt.Println("World")
 }
+
+/* As function executes line by line this will also execute line by line
+Output: Hello
+        World
+*/
