7e700a1bf91422f75cb4b8cae7b6aa7c4946ae8b
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-04-12 19:02:16 +0530

    Goroutune Example 6

--- main.go
+++ main.go
@@ -2,51 +2,21 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"time"
 )
 
-type shape interface {
-	Area() float64
-}
-
-type rectangle struct {
-	width, height float64
-}
-
-func (r rectangle) Area() float64 {
-	return r.width * r.height
-}
-
-type Circle struct {
-	radius float64
-}
-
-func (c Circle) Area() float64 {
-	return math.Pi * c.radius * c.radius
-}
-
-func calculateArea(s shape) float64 {
-	return s.Area()
+func sayHello() {
+	for i := 1; i <= 3; i++ {
+		fmt.Println("Hello", i)
+		time.Sleep(time.Millisecond * 500)
+	}
 }
 
 func main() {
-	rect := rectangle{height: 5, width: 4}
-	circle := Circle{radius: 2}
+	go sayHello() // runs in background
 
-	fmt.Println(
-		"Rectangle Area:",
-		calculateArea(rect),
-	)
-
-	fmt.Println(
-		"Circle Area:",
-		calculateArea(circle),
-	)
-
-	mysterBox := interface{}(10)
-	describeValue(mysterBox)
-}
-
-func describeValue(t interface{}) { // empty interface can accept any type
-	fmt.Printf("Type: %T, Value: %v\n", t, t)
+	for i := 1; i <= 3; i++ {
+		fmt.Println("Main", i)
+		time.Sleep(time.Millisecond * 500)
+	}
 }
